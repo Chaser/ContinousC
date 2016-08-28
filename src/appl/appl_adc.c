8d0bfe6b5828e88746822d2e3f0a35d0d67d0947
Chase Johnson <chaseajohnson@gmail.com>
2016-08-28 19:46:51 +0100

    Addition of App ADC and tests with mocking.

---
+++ src/appl/appl_adc.c
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ *    Description:   See header file
+ *
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ *    INCLUDED FILES
+ ******************************************************************************/
+
+#include "appl_adc.h"
+#include "appl_adc_channels.h"
+#include "bsp_adc.h"
+
+#include "xc.h"
+
+/*******************************************************************************
+ *    DEFINITIONS
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    PRIVATE FUNCTION PROTOTYPES
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    PRIVATE TYPES
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    PRIVATE DATA
+ ******************************************************************************/
+
+static T_ADCHandler _adc_handlers[ APPL_ADC_CH_CNT ];
+
+
+/*******************************************************************************
+ *    PUBLIC DATA
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    EXTERNAL DATA
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    EXTERNAL FUNCTION PROTOTYPES
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    PRIVATE FUNCTIONS
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    PROTECTED FUNCTIONS
+ ******************************************************************************/
+
+/**
+ * For usage in tests only!
+ */
+T_ADCHandler *_appl_adc__adc_handler__get(enum E_ApplAdcChannel channel)
+{
+    return &_adc_handlers[channel];
+}
+
+
+
+/*******************************************************************************
+ *    PUBLIC FUNCTIONS
+ ******************************************************************************/
+
+void appl_adc__init(void)
+{
+    enum E_ApplAdcChannel channel;
+
+    //-- init all ADC channels
+    for (channel = 0; channel < APPL_ADC_CH_CNT; channel++){
+        T_ADCHandler_CtorParams params = {};
+
+        //-- here, we initialize all channels with the same params,
+        //   but in real life different ADC channels may, of course,
+        //   have different parameters.
+        params.max_counts = 0x3ff;
+        params.bsp_max_voltage = 10/*V*/ * ADC_HANDLER__SCALE_FACTOR__U;
+        params.nominal_add_volts = 0/*V*/;
+
+        //-- construct the ADC handler, saving the result to _ctor_result
+        adc_handler__ctor(&_adc_handlers[ channel ], &params);
+    }
+}
+
+T_ADCHandler_Voltage appl_adc__voltage__get(enum E_ApplAdcChannel channel_num)
+{
+    T_ADCHandler_Voltage ret = 0;
+
+    if (channel_num < APPL_ADC_CH_CNT){
+        ret = adc_handler__voltage__get_by_counts_value(
+                &_adc_handlers[ channel_num ],
+                bsp_adc__value__get(channel_num)
+                );
+    } else {
+        //-- illegal channel_num given: should never be here
+        __builtin_software_breakpoint();
+    }
+
+    return ret;
+}
+
---
+++ test/test_appl_adc.c
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ *    INCLUDED FILES
+ ******************************************************************************/
+
+//-- unity: unit test framework
+#include "unity.h"
+
+//-- module being tested
+#include "appl_adc.h"
+
+//-- mocked modules
+#include "mock_bsp_adc.h"
+#include "mock_adc_handler.h"
+#include "mock_xc.h"
+
+
+/*******************************************************************************
+ *    DEFINITIONS
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    PRIVATE TYPES
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    EXTERNAL FUNCTION PROTOTYPES
+ ******************************************************************************/
+
+extern T_ADCHandler *_appl_adc__adc_handler__get(enum E_ApplAdcChannel channel);
+
+
+
+
+/*******************************************************************************
+ *    PRIVATE DATA
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ *    PRIVATE FUNCTIONS
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ *    SETUP, TEARDOWN
+ ******************************************************************************/
+
+void setUp(void)
+{
+    //-- ADC handler constructor is going to be called for each channel.
+    //   Mocked constructors all return ADC_HANDLER_RES__OK.
+    enum E_ApplAdcChannel channel;
+    for (channel = 0; channel < APPL_ADC_CH_CNT; channel++){
+        adc_handler__ctor_IgnoreAndReturn(ADC_HANDLER_RES__OK);
+    }
+
+    //-- before each test, re-initialize appl_adc module
+    appl_adc__init();
+}
+
+void tearDown(void)
+{
+    //-- nothing to do here
+}
+
+
+
+/*******************************************************************************
+ *    TESTS
+ ******************************************************************************/
+
+void test_voltage_get(void)
+{
+    //-- We expect bsp_adc__value__get() to be called:
+    bsp_adc__value__get_ExpectAndReturn(
+            //-- the argument that is expected to be given to
+            //   bsp_adc__value__get()
+            APPL_ADC_CH__I_SETT,
+
+            //-- and the value that bsp_adc__value__get() should return
+            123
+            );
+
+    //-- Expect call to adc_handler__voltage__get_by_counts_value(),
+    //   ignoring arguments. The mocked version just returns 456.
+    adc_handler__voltage__get_by_counts_value_ExpectAndReturn(
+            //-- pointer to the appropriate ADC handler instance
+            _appl_adc__adc_handler__get(APPL_ADC_CH__I_SETT),
+            //-- value returned from bsp_adc__value__get()
+            123,
+            //-- returned value in Volts
+            456
+            );
+
+    //-- actually call the function being tested, that should perform
+    //   all pending expected calls
+    T_ADCHandler_Voltage voltage = appl_adc__voltage__get(
+            APPL_ADC_CH__I_SETT
+            );
+
+    //-- check the voltage returned (it should be 456 from the mock above)
+    TEST_ASSERT_EQUAL_INT(456, voltage);
+}
+
+void test_voltage_get_wrong_channel_number(void)
+{
+    //-- we expect __builtin_software_breakpoint() to be called ...
+    __builtin_software_breakpoint_Expect();
+
+    //-- ... when we call appl_adc__voltage__get() with illegal
+    //   channel number.
+    appl_adc__voltage__get(
+            APPL_ADC_CH_CNT
+            );
+}
